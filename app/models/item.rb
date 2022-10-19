707f2291cd5dc8eac5ae0f9f9134a4f9a1a8f3e4
Hashiguchi0828 <ckonedized08@gmail.com>
2022-10-19 17:55:10 +0900

    active　hash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
+
+  validates :name, :description, :category_id, :status_id, :postage_id, :shipping_date_id, :price, :prefecture_id, :image, presence: true
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" } 
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
