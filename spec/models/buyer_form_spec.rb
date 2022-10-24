e50f96c48f09841b1b1afde730c07675ce0ca4b7
Hashiguchi0828 <ckonedized08@gmail.com>
2022-10-24 19:15:10 +0900

    修正

--- spec/models/buyer_form_spec.rb
+++ spec/models/buyer_form_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BuyerForm, type: :model do
   before do
-    @buyer_form = FactoryBot.build(:buyer_form)
+    @buyer_form = FactoryBot.build(:buyer_form, user_id: user.id, item_id: item.id)
   end
 
   describe '配送先情報の保存' do
