b55caa226206a69e67eca106b325348f0087af62
Hashiguchi0828 <ckonedized08@gmail.com>
2022-10-24 18:55:08 +0900

    10桁も保存できるように修正

--- app/models/buyer_form.rb
+++ app/models/buyer_form.rb
@@ -9,7 +9,7 @@ class BuyerForm
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
-    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
     validates :token, presence: true
   end
 
